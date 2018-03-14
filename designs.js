04ab3b06b898894da55bd718f92e8dedaf7f17fb
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-03-14 18:28:44 -0500

    udacity pixel art maker project

--- designs.js
+++ designs.js
@@ -1,10 +1,57 @@
-// Select color input
-// Select size input
+/*jshint esversion:6*/
+$(function() {
+  // Select inputs
+  let colorPicker = $("#colorPicker");
+  let heightInput = $("#input_height");
+  let widthInput = $("#input_width");
 
-// When size is submitted by the user, call makeGrid()
+  // select canvas
+  const canvas = $("#pixel_canvas");
 
-function makeGrid() {
+  // Call makeGrid()
+  function makeGrid() {
+    canvas.find("tbody").remove();
 
-// Your code goes here!
+    // "submit" the size form to update the grid size
+    let gridRows = heightInput.val();
+    let gridCol = widthInput.val();
 
-}
+    // set tbody to the table
+    canvas.append("<tbody></tbody>");
+
+    let canvasBody = canvas.find("tbody");
+
+    // draw grid row
+    for (let i = 0; i < gridRows; i++) {
+      canvasBody.append("<tr></tr>");
+    }
+
+    // draw grid col
+    for (let i = 0; i < gridCol; i++) {
+      canvas.find("tr").append('<td class="transparent"></td>');
+    }
+  }
+
+  $(document).ready(function() {
+    // click the submit button to update the grid
+    $('input[type="submit"]').on("click", function(e) {
+      e.preventDefault();
+      makeGrid();
+    });
+
+    // toggle grid color
+    $("#pixel_canvas").on("click", "td", function(e) {
+      let color = colorPicker.val();
+      let currentColor = $(this).css("background-color");
+      console.log(currentColor + " color:" + color);
+
+      if ($(this).hasClass("transparent")) {
+        $(this).toggleClass("transparent");
+        $(this).css("background-color", color);
+      } else {
+        $(this).toggleClass("transparent");
+        $(this).css("background-color", "transparent");
+      }
+    });
+  });
+});
